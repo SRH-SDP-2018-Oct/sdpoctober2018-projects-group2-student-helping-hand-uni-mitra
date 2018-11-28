36a7cc2b182aaa5987759fbb43d6fd87d9f88be6
Hitesh chaudhari <xabc061@gmail.com>
2018-11-29 00:33:29 +0100

    model class

--- src/main/java/com/unimitra/model/EventsModel.java
+++ src/main/java/com/unimitra/model/EventsModel.java
@@ -1,5 +1,18 @@
 package com.unimitra.model;
 
-public class EventsModel {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class EventsModel {
+	private int eventid;
+	private String eventName;
+	private String eventDescription;
+	private String eventCategory;
+	private String eventLocation;
+	private String eventCharges;
+	private String eventCreatedBy;
+	private String eventDateTime;
+	
 }
