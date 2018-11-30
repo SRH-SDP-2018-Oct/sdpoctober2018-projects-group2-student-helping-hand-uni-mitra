6d77b8c24db1736e86f4de172ce51aaf5dfda831
Shardulsonar <shardulsonar@gmail.com>
2018-11-30 10:03:32 +0100

    Adding Logging Configurations

--- src/main/java/com/unimitra/controller/EventController.java
+++ src/main/java/com/unimitra/controller/EventController.java
@@ -3,7 +3,8 @@ package com.unimitra.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +18,14 @@ import com.unimitra.service.EventsService;
 @RequestMapping("/events")
 public class EventController {
 
-	private static final Logger LOGGER = Logger.getLogger(EventController.class);
+	private static final Logger LOGGER = LogManager.getLogger();
 	@Autowired
 	EventsService eventsService;
 
 	@GetMapping("/get-details")
 	public List<EventsEntity> getEventDetails() {
 		List<EventsEntity> eventList = eventsService.getEventDetails();
-		LOGGER.info(eventList.toString());
+		LOGGER.info("UNIMITRA-AUDIT " + eventList.toString());
 		return ObjectUtils.isEmpty(eventList) ? new ArrayList<>() : eventList;
 	}
 
