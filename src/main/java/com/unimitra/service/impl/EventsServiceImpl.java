6cdf84284cb3d0fd5b921eaf4eb312009fd299a8
manasiDivekar <divekar.manasi01@gmail.com>
2018-12-02 19:34:33 +0530

    Ãevents apis

--- src/main/java/com/unimitra/service/impl/EventsServiceImpl.java
+++ src/main/java/com/unimitra/service/impl/EventsServiceImpl.java
@@ -1,10 +1,13 @@
 package com.unimitra.service.impl;
 
+import java.sql.Timestamp;
 import java.util.List;
 
+import javax.crypto.spec.PSource;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.ParsedSql;
 import org.springframework.stereotype.Service;
 
 import com.unimitra.dao.EventDao;
@@ -19,8 +22,40 @@ public class EventsServiceImpl implements EventsService {
 	EventDao eventsDao;
 
 	@Override
-	public List<EventsEntity> getEventDetails() {
+	public List<EventsEntity> getEventDetails() {		
 		return eventsDao.getEventDetails();
 	}
+
+	@Override
+	public EventsEntity getEventDetailById(int eventId) {
+		return eventsDao.getEventDetailById(eventId);
+	}
+
+	@Override
+	public String deleteEventById(int eventId) {
+		String deleteEventbyId = eventsDao.deleteEventById(eventId);
+
+		return deleteEventbyId;
+
+	}
+
+	@Override
+	public EventsEntity postEvent(EventsEntity postEvent) {
+		Timestamp time = new Timestamp(System.currentTimeMillis());
+		postEvent.setEventCreationDate(time);
+		postEvent.setEventCreationDate(time);
+		postEvent = eventsDao.postEvent(postEvent);
+		
+		return postEvent;
+	}
+
+/*	@Override
+	public String registerForEvent(int eventId, int userId, boolean eventRegistrationFlag) {
+		
+		
+		String eventRegitrationFlag = eventsDao.registerForEvent(eventId, userId, eventRegistrationFlag);
+		
+		return eventRegitrationFlag;
+	}*/
 
 }
