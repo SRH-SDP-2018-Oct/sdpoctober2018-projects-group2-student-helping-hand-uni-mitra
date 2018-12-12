5d1af31e01ce9f23ab3250ed292e518e32aa2b4a
Shardulsonar <shardulsonar@gmail.com>
2018-12-12 03:19:24 +0100

    Adding code for bug fix in discussion service, email utility for unknown errors in exception handler, and unit test cases

--- src/main/java/com/unimitra/utility/UnimitraConstants.java
+++ src/main/java/com/unimitra/utility/UnimitraConstants.java
@@ -8,4 +8,10 @@ public class UnimitraConstants {
 
 	public static final String UNI_MITRA_AUDIT = "UNIMITRA-AUDIT ";
 	public static final String UNI_MITRA_ERROR = "UNIMITRA-ERROR ";
+	public static final String POST_QUESTION = "post-question ";
+	public static final String ANSWER_QUESTION = "answer-question ";
+	public static final String DELETE_DISCUSSION = "delete-discussion ";
+	public static final String CLOSE_THREAD = "close-thread ";
+	public static final String SEARCH_DISCUSSION = "search-discussion ";
+	public static final String COMPLETED = "completed ";
 }
